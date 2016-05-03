5169145e714825ae7d3421ab70f275a73e7b753c
Michael P. Meyer <mmeyer@mdsol.com>
2016-05-03 10:00:54 -0700

    make raw_cas optional

--- plugin.js
+++ plugin.js
@@ -32,6 +32,8 @@ const Boom = require('boom');
  *  manager uses for tracking session identifiers.
  * @property {boolean} [strictSSL=true] Determines if the client will require
  *  valid remote SSL certificates or not.
+ * @property {boolean} [saveRawCAS=false] If true the CAS result will be
+ *  saved into session.raw_cas
  */
 
 const optsSchema = Joi.object().keys({
@@ -42,7 +44,8 @@ const optsSchema = Joi.object().keys({
   localAppUrl: Joi.string().uri({scheme: ['http', 'https']}).required(),
   endPointPath: Joi.string().regex(/^\/[\w\W\/]+\/?$/).required(),
   includeHeaders: Joi.array().items(Joi.string()).default(['cookie']),
-  strictSSL: Joi.boolean().default(true)
+  strictSSL: Joi.boolean().default(true),
+  saveRawCAS: Joi.boolean().default(false)
 });
 
 /**
@@ -102,8 +105,10 @@ function casPlugin(server, options) {
         request.session.username = result.user;
         request.session.attributes = result.attributes || {};
 
-		// Save raw cas result for processing by client
-        request.session.raw_cas = result;
+        // Save raw cas result for processing by client
+        if (_options.value.saveRawCAS) {
+          request.session.raw_cas = result;
+        }
 
         return addHeaders(request, reply(result).redirect(redirectPath));
       })
